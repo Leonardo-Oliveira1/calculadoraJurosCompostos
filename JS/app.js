cc4637f45a9187eadae5fa86053d772dacbd5275
Leonardo-Oliveira1 <oleonardo78@gmail.com>
2023-03-27 17:18:21 -0300

    Adicionando máscaras de moeda e corrigindo o bug do NaN no resultado

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,5 @@
-"use strict";
-var initial_value_input = document.getElementById("initial_value");
-var monthly_value_input = document.getElementById("monthly_value");
 var interest_rate_input = document.getElementById("interest_rate");
+
 var period_input = document.getElementById("period");
 var calculate_input = document.getElementById("calculate");
 var interest_rate_time = document.getElementById("interest_rate_time");
@@ -9,14 +7,28 @@ var period_time = document.getElementById("period_time");
 var final_total = document.getElementById("final_total");
 var total_invested = document.getElementById("total_invested");
 var total_interest_rate = document.getElementById("total_interest_rate");
+
 calculate_input.addEventListener("click", function () {
-    var initialValue = parseFloat(initial_value_input.value) || 0;
-    var monthlyValue = parseFloat(monthly_value_input.value) || 0;
-    var interestRate = parseFloat(interest_rate_input.value) || 0;
-    var period = parseInt(period_input.value) || 0;
-    var monthlySum = initialValue + (monthlyValue * period) || 0;
+    var initialValue = parseFloat(removeStyleCurrency(document.getElementById("initial_value").value));
+    var monthlyValue = parseFloat(removeStyleCurrency(document.getElementById("monthly_value").value));
+    var interestRate = parseFloat(interest_rate_input.value);
+    var period = parseInt(period_input.value);
+
+    var monthlySum = initialValue + (monthlyValue * period);
     var interestRateTime = interest_rate_time.value;
     var periodTime = period_time.value;
+
+    if(isNaN(monthlyValue)){
+        monthlyValue = 0;
+    }
+
+    if(isNaN(initialValue)){
+        initialValue = 0;
+    }
+       
+    console.clear()
+    console.log("Valor inicial: " + initialValue);
+    console.log("Valor mensal: " + monthlyValue);
     //TIME MEASUREMENTS
     switch (periodTime) {
         case "period_year":
@@ -27,15 +39,28 @@ calculate_input.addEventListener("click", function () {
             break;
         case "period_daily":
             period = period / 30;
-            console.log("Diario");
             break;
     }
+
+    if(isNaN(period)){
+        period = 0;
+    }
+
+    console.log("Periodo: " + period);
+
     if (interestRateTime == "rate_year") {
         interestRate = interestRate / 12;
     }
     else {
         interestRate = parseFloat(interest_rate_input.value);
     }
+
+    if(isNaN(interestRate)){
+        interestRate = 0;
+    }
+
+    console.log("Taxa: " + interestRate);
+
     //CALCULATION OF COMPOUND INTEREST
     var tax = (1 + (interestRate / 100));
     var amount = (monthlySum * Math.pow(tax, period)) || 0;
@@ -43,8 +68,21 @@ calculate_input.addEventListener("click", function () {
     if (totalInterestRate < 0) {
         totalInterestRate = 0;
     }
+
+    if(isNaN(monthlySum)){
+        monthlySum = 0;
+    }
+    
     //VALUES OUTPUT
     final_total.innerText = "".concat(amount.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
     total_invested.innerText = "".concat(monthlySum.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
     total_interest_rate.innerText = "".concat(totalInterestRate.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
 });
+
+function removeStyleCurrency(value){
+    var remove_comma = value.replace(".", "");
+    var replaceToDot = remove_comma.replace(",", ".");
+    var formatted = replaceToDot.replace("R$ ", "");
+
+    return formatted;
+}
